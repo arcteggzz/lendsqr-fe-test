0c8fb9603fd3f69f143b469f4966644bc742f1ee
Oghenetega Esedere <arcteggzz@gmail.com>
2023-03-21 08:08:17 +0100

    feat: added mock landingPage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,24 +1,32 @@
 import React, { useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import styles from "./LandingPage.module.scss";
+import assessment_image from "../assets/images/assessment_image.png";
 
 const LandingPage = () => {
   return (
     <>
       <div className={styles.LandingPage}>
-        <h1>LandingPage</h1>
-        <Link to={`/login`} className={styles.link}>
-          Login
-        </Link>
-        <Link to={`/dashboard`} className={styles.link}>
-          Dashboard
-        </Link>
-        <Link to={`/dashboard/users`} className={styles.link}>
-          users
-        </Link>
-        <Link to={`/dashboard/users/gega`} className={styles.link}>
-          UserDetails
-        </Link>
+        <section>
+          <h1>
+            Esedere Oghenetega’s <span>Submission for lendsqr Assessment</span>
+          </h1>
+          <div className={styles.description}>
+            <p>Task Description:</p>
+            <p>
+              Created a mini application for a user to login and view a list of
+              users fetched from an API and viewed details about each user.
+            </p>
+            <p>
+              Kindly note that all assests and designs used belong to Lengsqr.
+              This is part of the LENDSQR assessment task.
+            </p>
+          </div>
+          <Link to="/login" className={styles.linkLogin}>
+            CLICK TO VIEW SUBMISSION
+          </Link>
+        </section>
+        <img src={assessment_image} alt="assessment_image" />
       </div>
     </>
   );
